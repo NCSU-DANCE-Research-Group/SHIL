339a571d9315ccfda29c16d2b2e89a3e6b059909
Yuhang Lin <ylin34@ncsu.edu>
2022-07-14 15:57:51 -0700

    Remove unused model and update commands

--- verify.sh
+++ verify.sh
@@ -13,25 +13,25 @@
 # mv detected.csv predicted.csv testing-res.csv thresholds.csv recon_errors.csv testing-res-formatted.csv $dir 
 
 # Outlier detection
-# python3 outlier_detection_IsolationForest_similar.py
-# dir=data/label_using_outlier
-# mkdir -p $dir
-# for container in {1..4}
-# do
-#     mv outlier_$container.csv nonoutlier_$container.csv $dir
-# done
+python3 outlier_detection_IsolationForest_nonoutlier_normal.py
+dir=data/label_using_outlier
+mkdir -p $dir
+for container in {1..4}
+do
+    mv outlier_$container.csv nonoutlier_$container.csv $dir
+done
 
 # Self-supervised random forest model (used in SHIL)
-# python3 supervised_binary_randomforest_training.py
-# for confidence in 0.6
-# do
-#     python3 supervised_binary_randomforest_testing.py $confidence
-#     python3 format_res_csv.py
+python3 supervised_binary_randomforest_training.py
+for confidence in 0.6
+do
+    python3 supervised_binary_randomforest_testing.py $confidence
+    python3 format_res_csv.py
 
-#     dir=result/supervisedRF/RF-$confidence
-#     mkdir -p $dir
-#     mv detected.csv predicted.csv testing-res.csv testing-res-formatted.csv probabiltity.csv $dir
-# done
+    dir=result/supervisedRF/RF-$confidence
+    mkdir -p $dir
+    mv detected.csv predicted.csv testing-res.csv testing-res-formatted.csv probabiltity.csv $dir
+done
 
 # Self-supervised CNN model (alternative model, not used)
 # python3 supervised_CNN_training.py
